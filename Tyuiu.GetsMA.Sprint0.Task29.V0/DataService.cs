4b9b4509a2837cd79eb24cebd3e00925382f8628
Mariya1907 <marigets@gmail.com>
2025-11-26 22:26:14 +0500

    Task29

---
+++ Tyuiu.GetsMA.Sprint0.Task29.V0/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.GetsMA.Sprint0.Task29.V0.Lib
+{
+    public class DataService
+    {
+        public int Calculate()
+        {
+            return 2 * 9 + 7 * 2;
+        }
+    }
+}
+
