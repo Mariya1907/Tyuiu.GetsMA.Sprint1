ea985e829e005d57ae2f591d98d5e1341f7b982e
Mariya <Mariya@>
2025-12-09 15:18:57 +0500

    папап

--- Tyuiu.GetsMA.Sprint1.Task0.V29/Program.cs
+++ Tyuiu.GetsMA.Sprint1.Task0.V29/Program.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using Tyuiu.GetsMA.Sprint1.Task0.V29.Lib;
-
-
+﻿using Tyuiu.GetsMA.Sprint1.Task0.V29.Lib;
 
 internal class Program
 {
     static void Main()
     {
         ISprint1Task0V29 ds = new DataService();
-        int result = ds.Calculate();
-
-        // Только число, без слов
-        Console.WriteLine(result);
+        Console.WriteLine(ds.Calculate());
     }
 }
-
-
