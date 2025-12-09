9292b3292837cc707641ed5fff8214811401f267
Mariya <Mariya@>
2025-12-09 15:26:51 +0500

    рпвравра

--- Tyuiu.GetsMA.Sprint1.Task0.V29.Lib/DataService.cs
+++ Tyuiu.GetsMA.Sprint1.Task0.V29.Lib/DataService.cs
@@ -1,16 +1,11 @@
 ﻿namespace Tyuiu.GetsMA.Sprint1.Task0.V29.Lib
 {
-    public interface ISprint1Task0V29
-    {
-        int Calculate();
-    }
-
     public class DataService : ISprint1Task0V29
     {
         public int Calculate()
         {
-            // ВСЕ вычисления только здесь
             return 2 * 9 + 7 * 2;
         }
     }
 }
+
