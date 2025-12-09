bdac16094f5a8b748fdd799074b78e23da1a67c7
Mariya <Mariya@>
2025-12-09 14:42:22 +0500

    рррр

--- Tyuiu.GetsMA.Sprint1.Task0.V29.Lib/DataService.cs
+++ Tyuiu.GetsMA.Sprint1.Task0.V29.Lib/DataService.cs
@@ -1,10 +1,11 @@
 ﻿namespace Tyuiu.GetsMA.Sprint1.Task0.V29.Lib
 {
-    public class DataService
+    // DataService реализует интерфейс ISprint1Task0V29
+    public class DataService : ISprint1Task0V29
     {
         public int Calculate()
         {
-            return 2 * 9 + 7 * 2;
+            return 2 * 9 + 7 * 2; // Ваше вычисление
         }
     }
 }
--- Tyuiu.GetsMA.Sprint1.Task0.V29/Program.cs
+++ Tyuiu.GetsMA.Sprint1.Task0.V29/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using Tyuiu.GetsMA.Sprint1.Task0.V29.Lib;
 
+
+ISprint1Task0V29 service = new DataService();
+int result = service.Calculate();
+Console.WriteLine("Ответ: " + result);
+
 namespace Tyuiu.GetsMA.Sprint1.Task0.V29
 {
     internal class Program
