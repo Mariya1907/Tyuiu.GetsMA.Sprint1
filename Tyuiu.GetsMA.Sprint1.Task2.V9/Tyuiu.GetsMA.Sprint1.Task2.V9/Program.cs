22362a1ef1a3413b8b7c066118b2f463e88738d8
Mariya <Mariya@>
2025-12-11 02:35:52 +0500

    Добавлен проект Task3.V9: расчёт объёма шара

---
+++ Tyuiu.GetsMA.Sprint1.Task2.V9/Tyuiu.GetsMA.Sprint1.Task2.V9/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GetsMA.Sprint1.Task3.V9.Lib;
+using System;
+
+var dataService = new DataService();
+
+Console.Write("Введите радиус шара: ");
+int radius = Convert.ToInt32(Console.ReadLine());
+
+try
+{
+    double volume = dataService.CalculateSphereVolume(radius);
+    Console.WriteLine($"Объём шара: {volume}");
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"Ошибка: {ex.Message}");
+}
