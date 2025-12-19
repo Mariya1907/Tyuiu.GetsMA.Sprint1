f6ffb80f343709378909eb23c002cca6298748fa
GetsMariya <marigets@gmail.com>
2025-12-19 13:12:37 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,18 @@
-﻿using Tyuiu.GetsMA.Sprint1.Task2.V9.Lib;
-using System;
+﻿using Tyuiu.GetsMA.Sprint2.Task3.V19.Lib;
 
-var dataService = new DataService();
+ChessService cs = new ChessService();
 
-Console.Write("Введите радиус шара: ");
-int radius = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите x1 (1-8): ");
+int x1 = Convert.ToInt32(Console.ReadLine());
 
-try
-{
-    double volume = dataService.CalculateSphereVolume(radius);
-    Console.WriteLine($"Объём шара: {volume}");
-}
-catch (ArgumentException ex)
-{
-    Console.WriteLine($"Ошибка: {ex.Message}");
-}
+Console.Write("Введите y1 (1-8): ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите x2 (1-8): ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите y2 (1-8): ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+bool canMove = cs.CanBishopMove(x1, y1, x2, y2);
+Console.WriteLine(canMove);
