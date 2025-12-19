9da53ca444c4c8de2c1f0616ebbbf365b02acdb5
Mariya <Mariya@>
2025-12-19 20:07:09 +0500

    Add .gitignore and new tasks

---
+++ Tyuiu.GetsMA.Sprint1.Task4/Tyuiu.GetsMA.Sprint1.Task4.V14.Lib/Class1.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.Cources.Programming.Interfaces.Sprint1;
+
+namespace Tyuiu.GetsMA.Sprint1.Task4.V14.Lib
+{
+    public class DataService : ISprint1Task4V14
+    {
+        public double Calculate(double x, double y)
+        {
+            double numerator = Math.Sqrt(7 + Math.Abs(x - y));
+            double denominator = 3 * x * y * y;
+            double result = numerator / denominator;
+            return Math.Round(result, 3);
+        }
+    }
+}
---
+++ Tyuiu.GetsMA.Sprint1.Task4/Tyuiu.GetsMA.Sprint1.Task4.V14.Lib/ISprint1Task4V14.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.Cources.Programming.Interfaces.Sprint1
+{
+    public interface ISprint1Task4V14
+    {
+        double Calculate(double x, double y);
+    }
+}
