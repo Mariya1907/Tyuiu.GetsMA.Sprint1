77eb8076ad04c56b8596e0eb3f729fa1ae4a9f56
Mariya <Mariya@>
2025-12-19 20:37:39 +0500

    Task 5

---
+++ Tyuiu.GetsMA.Sprint1.Task5/Tyuiu.GetsMA.Sprint1.Task5.V1.Lib/ISprint1Task5V1.cs
@@ -0,0 +1,8 @@
+﻿namespace Tyuiu.cources.programming.interfaces.Sprint1
+{
+    public interface ISprint1Task5V1
+    {
+        int DistanceBetweenPoints(double x1, double y1, double x2, double y2);
+    }
+}
+
---
+++ Tyuiu.GetsMA.Sprint1.Task5/Tyuiu.GetsMA.Sprint1.Task5.V1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Tyuiu.GetsMA.Sprint1.Task5.V1.Lib;
+
+namespace Tyuiu.GetsMA.Sprint1.Task5.V1.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты первой точки:");
+            Console.Write("x1 = ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y1 = ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите координаты второй точки:");
+            Console.Write("x2 = ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y2 = ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            DataService ds = new DataService();
+            int distance = ds.DistanceBetweenPoints(x1, y1, x2, y2);
+
+            Console.WriteLine($"Расстояние между точками: {distance}");
+            Console.ReadKey();
+        }
+    }
+}
