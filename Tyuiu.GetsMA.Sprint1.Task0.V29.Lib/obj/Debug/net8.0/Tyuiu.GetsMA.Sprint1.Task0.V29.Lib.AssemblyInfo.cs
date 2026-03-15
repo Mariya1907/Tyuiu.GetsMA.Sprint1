2c210ed2c04cedfe54f4f1bc87444d4bb3fa5f8b
Mariya <Mariya@>
2026-03-16 03:58:20 +0500

    0

--- Tyuiu.GetsMA.Sprint1.Task0.V29.Lib/obj/Debug/net8.0/Tyuiu.GetsMA.Sprint1.Task0.V29.Lib.AssemblyInfo.cs
+++ Tyuiu.GetsMA.Sprint1.Task0.V29.Lib/obj/Debug/net8.0/Tyuiu.GetsMA.Sprint1.Task0.V29.Lib.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tyuiu.GetsMA.Sprint1.Task0.V29.Lib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb2776acca80b91ccb0a056219ddd3b22eaba210")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4ab859c6fa026dcd09e967182562f0ac2dd84c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tyuiu.GetsMA.Sprint1.Task0.V29.Lib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tyuiu.GetsMA.Sprint1.Task0.V29.Lib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Tyuiu.GetsMA.Sprint1.Task0.V29/Program.cs
+++ Tyuiu.GetsMA.Sprint1.Task0.V29/Program.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.GetsMA.Sprint1.Task0.V29
         {
             DataService ds = new DataService();
 
-            Console.Title = "Спринт #1 | Выполнил: Тяжов Л. А. | ПКТб-24-1";
+            Console.Title = "Спринт #1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #1                                                               *");
             Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
