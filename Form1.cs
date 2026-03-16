d450ece43ffc6d76d20b8ff72c5df005aec9fda3
Mariya <Mariya@>
2026-03-16 12:19:10 +0500

    4444

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.GetsMA.Sprint1.Task4.V14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
