94ae9be5102df2d676cc79bb58dd22d29bc5b343
Mariya <Mariya@>
2026-03-16 14:12:18 +0500

    1

--- Tyuiu.GetsMA.Sprint1.Task1.V19.Lib/DataService.cs
+++ Tyuiu.GetsMA.Sprint1.Task1.V19.Lib/DataService.cs
@@ -1,19 +1,16 @@
-﻿using System;
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
 
 namespace Tyuiu.GetsMA.Sprint1.Task1.V19.Lib
 {
-    public interface IDataService
-    {
-        double Calculate(double x, double y);
-    }
-    public class DataService : IDataService
+    public class DataService : ISprint1Task1V19
     {
         public double Calculate(double x, double y)
         {
             if (x == 0 || y == 0)
-                throw new ArgumentException("x и y не могут быть равны 0, чтобы избежать деления на ноль.");
+                throw new ArgumentException("x и y не должны быть равны 0");
 
             return (x + 10 * y) / (x * y);
         }
     }
 }
+
--- Tyuiu.GetsMA.Sprint1.Task1.V19.Test/DataServiceTests.cs
+++ Tyuiu.GetsMA.Sprint1.Task1.V19.Test/DataServiceTests.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Tyuiu.GetsMA.Sprint1.Task1.V19.Lib;
-using System;
 
-namespace Tyuiu.GetsMA.Sprint1.Task0.V19.Test
+namespace Tyuiu.GetsMA.Sprint1.Task1.V19.Test
 {
     [TestClass]
     public class DataServiceTests
@@ -10,23 +9,23 @@ namespace Tyuiu.GetsMA.Sprint1.Task0.V19.Test
         [TestMethod]
         public void Calculate_ValidInput_ReturnsCorrectResult()
         {
-            IDataService dataService = new DataService();
+            // ❌ УБЕРИ IDataService!
+            DataService dataService = new DataService();  // ✅ ПРЯМО DataService!
             double x = 2;
             double y = 3;
-            double expected = (x + 10 * y) / (x * y); // (2 + 30)/6 = 5.3333
+            double expected = 5.333;
 
             double result = dataService.Calculate(x, y);
-
-            Assert.AreEqual(expected, result, 0.0001);
+            Assert.AreEqual(expected, result, 0.001);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
         public void Calculate_ZeroInput_ThrowsException()
         {
-            IDataService dataService = new DataService();
-            dataService.Calculate(0, 5);
+            DataService dataService = new DataService();  // ✅ ПРЯМО DataService!
+
+            Assert.ThrowsException<ArgumentException>(() =>
+                dataService.Calculate(0, 5));
         }
     }
-
 }
