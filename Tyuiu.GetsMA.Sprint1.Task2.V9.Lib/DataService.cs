35b83586bc4e2b74d1ca397029ef450a176fca1e
Mariya <Mariya@>
2026-03-16 13:50:27 +0500

    2

--- Tyuiu.GetsMA.Sprint1.Task2.V9.Lib/DataService.cs
+++ Tyuiu.GetsMA.Sprint1.Task2.V9.Lib/DataService.cs
@@ -1,9 +1,10 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
 namespace Tyuiu.GetsMA.Sprint1.Task2.V9.Lib
 {
     public class DataService : ISprint1Task2V9
     {
-        public double CalculateVolumeCircle(int r)
+        public double CalculateVolumeCircle(int r)  // ← Твоя формула!
         {
             return Math.Round((4.0 / 3 * Math.PI * Math.Pow(r, 3)), 3);
         }
